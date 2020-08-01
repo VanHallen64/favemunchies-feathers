ac18e4adfd53f8a15b0438524355626200e834e9
Allen Vanegas <allen64avd@gmail.com>
2020-08-01 02:03:22 -0700

    Added Friebase and Cloud Run deployments

--- frontend/src/feathers.js
+++ frontend/src/feathers.js
@@ -4,7 +4,7 @@ import rest from '@feathersjs/rest-client';
 const client = feathers();
 
 // YOU HAVE TO UPDATE THIS WITH YOUR OWN DOMAIN NAME
-const restClient = rest('http://localhost:8080');
+const restClient = rest('https://favemunchies.494902.xyz/');
 
 client.configure(restClient.fetch(window.fetch));
 client.configure(feathers.authentication({
