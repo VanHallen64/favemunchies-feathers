2e8d6a671138b29d55874ec2fe4c52571045f9c2
Allen Vanegas <allen64avd@gmail.com>
2020-08-01 04:36:42 -0700

    Fixed authentication

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
     render () {
         return (
             <div>
-                <h1>Welcome to</h1>
+                <h1>Welcome</h1>
 
             </div>
         );
--- frontend/src/feathers.js
+++ frontend/src/feathers.js
@@ -4,7 +4,7 @@ import rest from '@feathersjs/rest-client';
 const client = feathers();
 
 // YOU HAVE TO UPDATE THIS WITH YOUR OWN DOMAIN NAME
-const restClient = rest('https://favemunchies.494902.xyz/');
+const restClient = rest("favemunchies.web.app");
 
 client.configure(restClient.fetch(window.fetch));
 client.configure(feathers.authentication({
